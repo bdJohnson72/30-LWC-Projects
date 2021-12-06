f130c6a159b97ed883c4610518cd2ac0a358234b
Brooks Johnson <brooksjohnson157@gmail.com>
2021-12-06 08:16:08 -0500

    custom radio buttons

---
+++ force-app/main/default/lwc/customRadioButtons/customRadioButtons.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Brooks Johnson on 12/6/2021.
+ */
+
+/**
+ * Created by Brooks Johnson on 12/5/2021.
+ */
+
+import {LightningElement} from 'lwc';
+
+export default class CustomRadioButtons extends LightningElement {
+
+    invalid;
+    screen1 = true;
+    screen2;
+
+    handleClick(event){
+        const radioArray = Array.from(this.template.querySelectorAll('input'));
+        let valid = radioArray.filter((input) => input.checked);
+        if (valid.length === 0){
+            console.log('adding invalid classList')
+            radioArray[0].classList.add('invalid');
+            this.invalid = true;
+        }else {
+            this.screen1 = false;
+            this.screen2 = true;
+        }
+    }
+
+    handleRadioClick(event){
+        console.log(event.target)
+        const radioArray = Array.from(this.template.querySelectorAll('input'));
+        radioArray.forEach(radio => radio.classList.remove('invalid'))
+        this.invalid = false;
+    }
+}
